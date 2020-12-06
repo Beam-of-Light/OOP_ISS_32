61b68bfdb2c030ea037be05ff9c1facb50fc864b
Beam-of-Light <v.prockopchuck2015@gmail.com>
2020-12-07 00:18:29 +0200

    Update deposit class

--- Labs/Lab1/src/main/java/bankSystem/Deposit.java
+++ Labs/Lab1/src/main/java/bankSystem/Deposit.java
@@ -1,27 +1,67 @@
 package bankSystem;
 
 public class Deposit {
-    private String bankName;
-    private long startMoney;
+    private final String bankName;
     private long currentMoney;
-    private int percent;
-    private int monthsDuration;
+    private final int percent;
+    private final int monthsDuration;
     private int monthsLeft;
+    private final boolean earlyWithdrawal;
+    private final boolean rechargeable;
 
 
-    public Deposit(String bankName, long startMoney, int percent, int monthsDuration) {
+    public Deposit(String bankName, long startMoney, int percent, int monthsDuration,
+                   boolean earlyWithdrawal, boolean rechargeable) {
         this.bankName = bankName;
-        this.currentMoney = this.startMoney = startMoney;
-        this.percent = percent;
-        this.monthsLeft = this.monthsDuration = monthsDuration;
+        this.earlyWithdrawal = earlyWithdrawal;
+        this.rechargeable = rechargeable;
+
+        if (startMoney < 0) {
+            this.currentMoney = 0L;
+        } else {
+            this.currentMoney = startMoney;
+        }
+
+        if (percent < 0) {
+            this.percent = 0;
+        } else {
+            this.percent = percent;
+        }
+
+        if (monthsDuration < 0) {
+            this.monthsLeft = this.monthsDuration = 0;
+        } else {
+            this.monthsLeft = this.monthsDuration = monthsDuration;
+        }
+    }
+
+    public boolean putMoney(long money) {
+        if (rechargeable) {
+            currentMoney += money;
+        }
+        return rechargeable;
+    }
+
+    public long getMoney() {
+        if (monthsLeft == 0 || earlyWithdrawal) {
+            long money = currentMoney;
+            currentMoney = 0;
+            monthsLeft = 0;
+            return money;
+        } else {
+            return 0;
+        }
+    }
+
+    public void nextMonth() {
+        if (monthsLeft > 0) {
+            currentMoney += (currentMoney * percent) / 100;
+            --monthsLeft;
+        }
     }
 
     public String getBankName() {
         return bankName;
-    }
-
-    public long getStartMoney() {
-        return startMoney;
     }
 
     public long getCurrentMoney() {
@@ -38,5 +78,13 @@ public class Deposit {
 
     public int getMonthsLeft() {
         return monthsLeft;
+    }
+
+    public boolean isRechargeable() {
+        return rechargeable;
+    }
+
+    public boolean isEarlyWithdrawal() {
+        return earlyWithdrawal;
     }
 }
